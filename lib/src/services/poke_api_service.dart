cac73b9d756d7c5d9437d9708dafe0c0ed6f5dad
Charles <charleslanop@gmail.com>
2021-09-27 23:27:17 -0300

    add dialog error connection

--- lib/src/services/poke_api_service.dart
+++ lib/src/services/poke_api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:get/get.dart';
 import 'package:http/http.dart';
 import 'package:pokedex/src/constants/app_constants.dart';
 import 'package:pokedex/src/models/poke_model.dart';
@@ -16,8 +17,18 @@ class PokeApiService {
         final dynamic decodeJson = jsonDecode(response.body);
         return PokeModel.fromJson(decodeJson);
       }
+      await Get.defaultDialog<dynamic>(
+        barrierDismissible: false,
+        title: 'Error',
+        middleText: 'API error',
+      );
       throw Exception('API error');
     } on Exception catch (error) {
+      await Get.defaultDialog<dynamic>(
+        barrierDismissible: false,
+        title: 'Error',
+        middleText: 'Internet error: $error',
+      );
       throw Exception('Internet error: $error');
     }
   }
